05cd2d412a085a6e6e56d4dce3458fbb9aab559c
Yefremov Heorhii <yefremov.heorhii.job@gmail.com>
2023-07-27 21:45:40 +0300

    Chat GPT bot v1

--- HolidaysTelegramBot/Program.cs
+++ HolidaysTelegramBot/Program.cs
@@ -1,17 +1,20 @@
-﻿using Telegram.Bot;
+﻿using OpenAI_API;
+using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 var botClient = new TelegramBotClient("5914114434:AAGJ9mh8Iy92K4IBtoq6awzsZDrIscSOd5g");
 
+
 using CancellationTokenSource cts = new();
 
-// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+
 ReceiverOptions receiverOptions = new()
 {
-    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+    AllowedUpdates = Array.Empty<UpdateType>() 
 };
 
 botClient.StartReceiving(
@@ -26,15 +29,15 @@ var me = await botClient.GetMeAsync();
 Console.WriteLine($"Start listening for @{me.Username}");
 Console.ReadLine();
 
-// Send cancellation request to stop bot
+
 cts.Cancel();
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
-    // Only process Message updates: https://core.telegram.org/bots/api#message
+
     if (update.Message is not { } message)
         return;
-    // Only process text messages
+
     if (message.Text is not { } messageText)
         return;
 
@@ -42,11 +45,31 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
 
     Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
 
-    // Echo received message text
-    Message sentMessage = await botClient.SendTextMessageAsync(
-        chatId: chatId,
-        text: "You said:\n" + messageText,
-        cancellationToken: cancellationToken);
+    switch (message.Text)
+    {
+        case "Gpt":
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Write persone description",
+                replyMarkup: GetButtons(),
+                cancellationToken: cancellationToken);
+            break;
+        case "What this bot can do?":
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Bot will write a list of ideas for presents for birthday to someone. \nYou only have to write a description of this persone.",
+                replyMarkup: GetButtons(),
+                cancellationToken: cancellationToken);
+            break;
+        default:
+            string response = await AskChatGPT(message.Text);
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: response,
+                replyMarkup: GetButtons(),
+                cancellationToken: cancellationToken);
+            break;
+    }   
 }
 
 Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
@@ -60,4 +83,26 @@ Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, 
 
     Console.WriteLine(ErrorMessage);
     return Task.CompletedTask;
+}
+
+IReplyMarkup? GetButtons()
+{
+    return new ReplyKeyboardMarkup
+    (
+         new List<KeyboardButton>
+         (
+            new List<KeyboardButton> { new KeyboardButton("Gpt"), new KeyboardButton("What this bot can do?") }
+         )
+    );
+}
+
+async Task<string> AskChatGPT(string query)
+{
+    var openAI = new OpenAIAPI("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");
+    var chat = openAI.Chat.CreateConversation();
+    chat.AppendSystemMessage("You get a description of a person and have to write some ideas for present to this persone. Write only propositions for present to this persone.");
+    chat.AppendUserInput(query);
+    string response = await chat.GetResponseFromChatbotAsync();
+    Console.WriteLine(response);
+    return response;
 }
